c34dba061652bd290080d9c9bea06210164b5b39
Spandan Bandyopadhyay <spandan@maventic.com>
2026-03-30 07:33:30 +0000

    Flexible Column Layout Added & Custom Page(properly not responding) added

---
+++ app/project1/webapp/controller/CustomPage.controller.js
@@ -0,0 +1,20 @@
+const { func } = require("@sap/cds/lib/ql/cds-ql");
+
+sap.ui.define([
+    "sap/fe/core/PageController",
+    "sap/m/MessageToast"
+], function (PageController,MessageToast) {
+    "use strict";
+
+    return PageController.extend("project1.controller.CustomPage", {
+
+        onInit: function () {
+            PageController.prototype.onInit.apply(this, arguments);
+        },
+
+        onPress : ()=>{
+            MessageToast.show("Button Clicked");
+        }
+
+    });
+});
